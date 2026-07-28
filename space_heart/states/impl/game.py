52e706fff7b950409c0cfbcd4fa3bc261588333f
Jiggly-Balls <jigglyballs9000@gmail.com>
2026-07-28 16:54:43 +0530

    Added demo game

---
+++ space_heart/states/impl/game.py
@@ -0,0 +1,6 @@
+from space_heart.states.meta import BaseState, StateEnum
+
+
+class GameState(BaseState, state_name=StateEnum.GAME):
+    def process_update(self, dt: float) -> None:
+        self.window.fill("purple")
