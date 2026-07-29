75d9e442eb6fb9b0943f1db62abc07ae69ad83f6
Jiggly-Balls <jigglyballs9000@gmail.com>
2026-07-29 13:36:13 +0530

    Initialized game loop

--- space_heart/__main__.py
+++ space_heart/__main__.py
@@ -1,10 +1,33 @@
+from typing import TYPE_CHECKING
+
 import pygame
+
+from space_heart.states.impl import GameState
+from space_heart.states.meta import BaseManager, BaseState, LoaderState, StateEnum
 
 
 def main() -> None:
     pygame.init()
     pygame.display.set_caption("Space Heart")
     window = pygame.display.set_mode((640, 360))
+    clock = pygame.time.Clock()
+
+    state_manager = BaseManager(
+        post_init_state=StateEnum.GAME, bound_state_type=BaseState, window=window
+    )
+    state_manager.load_states(LoaderState, GameState)
+    state_manager.change_state(StateEnum.LOADER_STATE)
+    if TYPE_CHECKING:
+        assert state_manager.current_state is not None
+
+    while state_manager.is_running:
+        dt = clock.tick(60.0) / 1000
+
+        for event in pygame.event.get():
+            state_manager.current_state.process_event(event, dt)
+        state_manager.current_state.process_update(dt)
+
+        pygame.display.update()
 
 
 if __name__ == "__main__":
