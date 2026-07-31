5ae3dc29ed98b315f695b86eb98515de0029ab97
Jiggly-Balls <jigglyballs9000@gmail.com>
2026-08-01 02:20:41 +0530

    Added space-like movement system

--- space_heart/entities/player.py
+++ space_heart/entities/player.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 
 import pygame
 
+from space_heart.core.const import SPACE_DRAG
+
 if TYPE_CHECKING:
     from pygame import Surface, Vector2
 
@@ -12,27 +14,27 @@ class Player:
     def __init__(self) -> None:
         self.direction: Vector2 = pygame.Vector2()
         self.camera: Vector2 = pygame.Vector2()
-        self.speed: float = 300
+        self.speed: float = 100
+        self.acceleration: float = 0.1
 
     def update(self, dt: float) -> None:
         key_pressed = pygame.key.get_pressed()
 
         if key_pressed[pygame.K_w]:
-            self.direction.y = -1
+            self.direction.y += -self.acceleration
         elif key_pressed[pygame.K_s]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
+            self.direction.y += self.acceleration
 
         if key_pressed[pygame.K_d]:
-            self.direction.x = 1
+            self.direction.x += self.acceleration
         elif key_pressed[pygame.K_a]:
-            self.direction.x = -1
-        else:
-            self.direction.x = 0
+            self.direction.x += -self.acceleration
 
         if self.direction.magnitude() != 0.0:
-            self.direction.normalize_ip()
+            self.direction *= SPACE_DRAG
+
+        if self.direction.magnitude() < 0.000001:
+            self.direction.update(0, 0)
 
         self.camera += self.direction * self.speed * dt
 
