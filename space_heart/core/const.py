0deaa04bb4576b90d2d0b4b8c81fe580e1ed12bd
Jiggly-Balls <jigglyballs9000@gmail.com>
2026-08-01 02:15:25 +0530

    Updated SPACE_LAYER and added SPACE_DRAG & Colour constants

--- space_heart/core/const.py
+++ space_heart/core/const.py
@@ -1,9 +1,34 @@
 from __future__ import annotations
 
-__all__ = ("GAME_TITLE", "WINDOW_HEIGHT", "WINDOW_WIDTH")
+import random
+from enum import Enum
+
+__all__ = ("GAME_TITLE", "WINDOW_HEIGHT", "WINDOW_WIDTH", "Colour")
 
 GAME_TITLE: str = "Space Heart"
 WINDOW_WIDTH: int = 960
 WINDOW_HEIGHT: int = 540
 
-SPACE_LAYERS: int = 4
+SPACE_LAYERS: int = 10
+SPACE_DRAG: float = 0.98
+
+
+class Colour(Enum):
+    ORANGE = (251, 84, 43)
+    RED = (255, 51, 81)
+    BLUE = (104, 200, 255)
+    TEAL = (52, 255, 206)
+    LIGHT_GREEN = (46, 255, 78)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
+    @staticmethod
+    def rand_star_colour() -> Colour:
+        return random.choice(
+            (
+                Colour.ORANGE,
+                Colour.BLUE,
+                Colour.TEAL,
+                Colour.WHITE,
+            )
+        )
