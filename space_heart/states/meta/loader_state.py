f8137e76b42705c700140502cf6409b79babe6cd
Jiggly-Balls <jigglyballs9000@gmail.com>
2026-08-06 13:39:27 +0530

    Added shader loading

--- space_heart/states/meta/loader_state.py
+++ space_heart/states/meta/loader_state.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from space_heart.core.const import DIR_SHADERS_FRAG, DIR_SHADERS_VERT, ShaderEnum
 from space_heart.states.meta.base_state import BaseState
 from space_heart.states.meta.state_enums import StateEnum
 
@@ -21,7 +22,23 @@ def _mark_to_load(func: LoaderFunc) -> LoaderFunc:
 
 class LoaderState(BaseState, state_name=StateEnum.LOADER_STATE):
     @_mark_to_load
-    def _generate_space_sheets(self) -> None: ...
+    def _load_shaders(self) -> None:
+        for shader in ShaderEnum:
+            frag_content: str | None = None
+            vert_content: str
+
+            if shader.value.fragment:
+                frag_path = DIR_SHADERS_FRAG / shader.value.fragment
+                with frag_path.open("r") as f:
+                    frag_content = f.read()
+
+            vert_path = DIR_SHADERS_VERT / shader.value.vertex
+            with vert_path.open("r") as f:
+                vert_content = f.read()
+
+            self.shaders[shader] = self.ctx.program(
+                vertex_shader=vert_content, fragment_shader=frag_content
+            )
 
     def on_enter(self, previous_state: BaseState | None) -> None:
         for func in _funcs_to_load:
